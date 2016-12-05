0d042a9646522d56728312e2b35c9d5088dff3b7
Xavier Roig Martín <maroxa86@gmail.com>
2016-12-05 16:33:14 +0100

    Realizacion ejercicio 5

---
+++ Java Intermedio/Ejercicio5/src/servidor/Servidor.java
@@ -0,0 +1,48 @@
+package servidor;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+
+	private ServerSocket servidor;
+	
+	public Servidor(){
+		try {
+			servidor = new ServerSocket(1234);
+		} catch (IOException e) {
+			System.err.println("Error al inicializar el servidor:\n" + e);
+			System.exit(1);
+		}
+	}
+	
+	public void startServer(){
+		try {
+			System.out.println("Esperando conexión.....");
+			while(true){
+				Socket cliente = servidor.accept(); //Accept comienza el socket y espera una conexión desde un cliente
+				System.out.println("Cliente conectado");
+				DataOutputStream salidaCliente = new DataOutputStream(cliente.getOutputStream());
+				salidaCliente.writeUTF("Peticion recibida y aceptada");
+
+				BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+				String mensajeServidor;
+				File fichero = new File("datos.txt");
+				FileWriter textOut = new FileWriter(fichero,true);
+				while((mensajeServidor = entrada.readLine()) != null){
+					textOut.write(mensajeServidor);
+				}
+				textOut.close();
+				servidor.close();
+			}
+		} catch (IOException e) {
+			System.err.println("Error en el servidor:\n" + e.getMessage());
+		}
+	}
+}
