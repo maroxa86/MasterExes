b2503efd2fee340008d839732ca8432591ab4784
Xavier Roig Martin <maroxa86@gmail.com>
2016-12-07 11:20:41 +0100

    Realización del ejercicio 6

--- Java Intermedio/Ejercicio6/src/Servidor/Servidor.java
+++ Java Intermedio/Ejercicio6/src/Servidor/Servidor.java
@@ -1,21 +1,25 @@
 package Servidor;
 
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Vector;
 
 public class Servidor {
 	
 	private ServerSocket servidor;
-	
+	private Vector<Socket> vectorClientes;
 	public Servidor(){
-		
+		vectorClientes = new Vector<>();
 	}
 	
 	public void arrancarServidor(){
 		try {
-			servidor = new ServerSocket(9000);
+			servidor = new ServerSocket(1234);
 		} catch (IOException e) {
 			System.err.println("Error al arrancar el servidor.\n" + e);
 			System.exit(1);
@@ -25,25 +29,50 @@ public class Servidor {
 		while(true){
 			try {
 				Socket cliente = servidor.accept();//Accept comienza el socket y espera una conexión desde un cliente
-				System.out.println("Cliente conectado");
-				DataOutputStream salidaCliente = new DataOutputStream(cliente.getOutputStream());
-				salidaCliente.writeUTF("Nuevo usuario en el chat\n");
-				
-				atenderUsuario(cliente);				
+				vectorClientes.add(cliente);
+				Thread hilo = new Thread(new Runnable(){
+
+					@Override
+					public void run() {
+						atenderCliente(cliente);
+					}
+					
+				});
+				hilo.start();
 			} catch (IOException e) {
 				System.err.println("Error en la ejecución del servidor.\n" + e);
-			} 	
-			finally{
-				try {
-					servidor.close();
-				} catch (IOException e) {
-					System.err.println("Error al cerrar el servidor.\n" + e);
-				}
-			}
+			} 
 		}
 	}
 
-	private void atenderUsuario(Socket cliente) {
-		
+	private void atenderCliente(Socket cliente) {
+		System.out.println("Cliente conectado");
+		try {
+			OutputStream os = cliente.getOutputStream();
+			os.write(("Bienvenido al chat" + System.getProperty("line.separator")).getBytes("ASCII"));
+			os.flush();
+			os.write("Tu Nombre de usuario es: " .getBytes("ASCII"));
+			os.flush();
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+			String nick = br.readLine();
+			
+			String linea = null;
+			while((linea = br.readLine()) != null){
+				System.out.println(linea);
+				for(Socket usuario : vectorClientes){
+					try{
+						OutputStream os2 = usuario.getOutputStream();
+						os2.write((("Usuario " + nick + " escribe: " + linea).toUpperCase() + System.getProperty("line.separator")).getBytes("ASCII"));
+						os2.flush();
+					}catch(IOException e){
+						System.err.println("Cliente Desconectado");
+						vectorClientes.remove(usuario);
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("Error en el cliente.\n" + e);
+		}
 	}
 }
