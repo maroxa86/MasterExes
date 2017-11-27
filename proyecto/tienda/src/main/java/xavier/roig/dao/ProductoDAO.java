8d44105d577fe57e96a8db6e1923b75f421ea7df
Xavier Roig Martín <maroxa86@gmail.com>
2017-11-27 19:31:41 +0100

    Eliminacion del proyecto

--- proyecto/tienda/src/main/java/xavier/roig/dao/ProductoDAO.java
+++
@@ -1,7 +0,0 @@
-package xavier.roig.dao;
-
-import xavier.roig.model.Producto;
-
-public interface ProductoDAO extends GenericDAO<Producto, Integer> {
-
-}
--- proyecto/tienda/src/main/java/xavier/roig/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package xavier.roig.service;
-
-import xavier.roig.model.Usuario;
-
-public interface LoginService {
-	Usuario login(String user, String password);
-}
